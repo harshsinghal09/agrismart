650585eee9a1dfd1ed7f744330b2a4f5afcebbea
harshsinghal09 <harshsinghal8899@gmail.com>
2026-04-25 18:53:00 +0530

    Update index.js

--- agrismart/server/index.js
+++ agrismart/server/index.js
@@ -10,7 +10,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Security middleware
 app.use(helmet());
